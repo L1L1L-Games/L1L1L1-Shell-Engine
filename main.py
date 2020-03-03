7140c2970a4d687989f1bad35957cd8bfe14b8ab
L1L1L-Games <jordashin2005@student.lvusd.org>
2020-03-03 04:49:07 +0000

    Added Exit Animation and [clear] command

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-from subprocess import call
-import os
+import L1L1L1Engine
+
 from time import sleep
 
-def clear(): 
-    # check and make call for specific operating system 
-    _ = call('clear' if os.name =='posix' else 'cls') 
+from L1L1L1Engine import clear, start
 
-print("L1L1L1 Shell Engine")
-print('_' * 25)
-print('Basic Syntax:')
-print('\t[exit] - exits the shell')
-print('\t[clear] - clears the screen')
-print('_' * 35)
-print()
+
 with open('code.xeq', 'w') as file:
   while True:
     try:
       command = input(">>")
       if(command == '[exit]'):
+        for i in range(1, 7):
+          clear()
+          c = i * '.'
+          print('Exiting Engine' + str(c))
+          sleep(0.5)
+        clear()
+        print("Exited")
         break
       if(command == '[clear]'):
         clear()
+        start()
       exec(command)
       file.write(command + '\n')
     except:
